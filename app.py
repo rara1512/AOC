19ee745ffddc3b64c206259e3e47d2bf5b0156c7
Rahul Ramaswamy <36606911+rara1512@users.noreply.github.com>
2023-10-01 21:22:57 +0000

    Modified graph

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ with st.expander("Apply filters"):
 
 # Filtered Charts
 node_type_rows = st.slider('How many node-types would you like to see?', 0, len(selected_atom_df["node-type"].value_counts()), 1)
-selected_atom_df = selected_atom_df["node-type"].value_counts().to_frame().reset_index().rename(columns = {'index':'node_type', 'node-type':'count'}).head(node_type_rows)
+selected_atom_df["node-type"].value_counts().to_frame().reset_index().rename(columns = {'index':'node_type', 'node-type':'count'}, inplace = True).head(node_type_rows)
 st.write(selected_atom_df)
 node_type_chart = px.bar(selected_atom_df, x='node_type', y='count', title=f"node_type and its count for {atom_selected}")
 st.plotly_chart(node_type_chart, use_container_width=True)
