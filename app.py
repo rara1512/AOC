01aa78ac18518743d86b610267bbc5357ca233be
Rahul Ramaswamy <36606911+rara1512@users.noreply.github.com>
2023-10-01 21:52:33 +0000

    fixed last graph

--- app.py
+++ app.py
@@ -41,6 +41,6 @@ selected_atom_df = selected_atom_df["node-type"].value_counts().to_frame().reset
 node_type_chart = px.bar(selected_atom_df, x='node-type', y='count', title=f"node-type and its count for {atom_selected}")
 st.plotly_chart(node_type_chart, use_container_width=True)
 
-# parent_type_rows = st.slider('How many parent-types would you like to see?', 0, len(selected_atom_df["parent-type"].value_counts()), 1)
-# parent_type_chart = px.bar(selected_atom_df["parent-type"].value_counts().to_frame().reset_index().rename(columns = {'index':'parent_type'}).head(parent_type_rows), x='parent_type', y='count', title=f"parent_type and its count for {atom_selected}")
-# st.plotly_chart(parent_type_chart, use_container_width=True)
+parent_type_rows = st.slider('How many parent-types would you like to see?', 0, len(selected_atom_df["parent-type"].value_counts()), 1)
+parent_type_chart = px.bar(selected_atom_df["parent-type"].value_counts().to_frame().reset_index().rename(columns = {'index':'parent_type'}).head(parent_type_rows), x='parent_type', y='count', title=f"parent_type and its count for {atom_selected}")
+st.plotly_chart(parent_type_chart, use_container_width=True)
